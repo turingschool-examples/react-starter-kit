841a77eaa0207c9f5618c8c55e8013dcf44a89f0
bekahlundy <lundyrebekah@gmail.com>
2016-12-27 15:00:00 -0600

    connect index.js to application.js

---
+++ lib/Application.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default class Application extends React.Component {
+  render() {
+    return(
+      <div>hey</div>
+    )
+  }
+}
