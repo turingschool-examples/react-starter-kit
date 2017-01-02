231343d6d453030ae0741023cb23af3f08196745
bekahlundy <lundyrebekah@gmail.com>
2017-01-02 15:11:54 -0700

    introduce skeleton

--- lib/Application.jsx
+++ lib/Application.jsx
@@ -4,7 +4,19 @@ import ReactDom from 'react-dom';
 export default class Application extends React.Component {
   render() {
     return(
-      <div>hey</div>
+      <div>
+        <header className='HeaderSection'> {this.props.number}
+        </header>
+        <section className='InputSection'>
+          {/* <InputSection /> */}
+        </section>
+        <section className='DisplaySection'>
+          {/* <DisplaySection /> */}
+        </section>
+        <section className='ResetSection'>
+          {/* <ResetSection /> */}
+        </section>
+      </div>
     )
   }
 }
