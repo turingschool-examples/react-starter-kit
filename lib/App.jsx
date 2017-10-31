36a0c6664527dca4bdb1fcb0c734e9f321e84c9d
Nathaniel Foster <nathanielsmusic@gmail.com>
2017-10-31 11:30:44 -0600

    updated App

--- lib/App.jsx
+++ lib/App.jsx
@@ -3,32 +3,47 @@ import Card from './Card';
 import Controls from './Controls';
 import apiResponse from './apiResponse';
 
-export default function App () {
-  const cleanData = apiResponse.results.map( (trivia) => {
-    return {
-      question: trivia.question,
-      answer: trivia.correct_answer
-    }
-  } )
+const cleanData = apiResponse.results.map( (trivia) => {
+  return {
+    question: trivia.question,
+    answer: trivia.correct_answer
+  }
+} )
 
+export default class App extends React.Component {
+  constructor() {
+    super();
 
-  return (
-    <div>
-      <h1>App Title</h1>
+    this.state = {};
+  }
 
-      <Controls />
-      
-      {
-        cleanData.map( (trivia, index) => {
-          return (
-            <Card 
-              key={index} 
-              question={trivia.question} 
-              answer={trivia.answer}
-            />
-          );
-        } )
-      }
-    </div>
-  )
+  filterData(filter) {
+    const filteredData = cleanData.filter( (trivia) => {
+      return trivia.question.includes( filter );
+    } );
+
+    return filteredData;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>App Title</h1>
+
+        <Controls />
+        
+        {
+          cleanData.map( (trivia, index) => {
+            return (
+              <Card 
+                key={index} 
+                question={trivia.question} 
+                answer={trivia.answer}
+              />
+            );
+          } )
+        }
+      </div>
+    )
+  }
 }
