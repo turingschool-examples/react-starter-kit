0eb8ba58fc74c779cfbc8813caf8f268ed0bd43d
Nathaniel Foster <nathanielsmusic@gmail.com>
2017-11-01 10:04:36 -0600

    started testing

---
+++ test/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from '../lib/App';
+
+
+describe('App', () => {
+  it('should shallow', () => {
+    const component = shallow(<App />);
+
+    console.log(component.debug());
+  })
+})
