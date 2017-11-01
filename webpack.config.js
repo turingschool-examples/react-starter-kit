36420ca875a28e08774a430d328522a830698f15
Nathaniel Foster <nathanielsmusic@gmail.com>
2017-11-01 08:49:25 -0600

    ignore css files in your react files

--- webpack.config.js
+++ webpack.config.js
@@ -29,4 +29,11 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
   },
+  jest: {
+    "moduleNameMapper": {
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+      "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    }
+  }
+}
 };
